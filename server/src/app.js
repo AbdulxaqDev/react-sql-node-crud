971932be5878002642bab8ef14e0b390958da9fd
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-06-11 23:59:29 +0300

    setting up server side

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 
+const studentsRouter = require("./routes/students/students.route.js");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+
+app.use("/students", studentsRouter);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,16 @@
 const http = require("http");
 
 const app = require("./app");
+const { connectDB } = require("./models/students.module.js");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`Server is running on ${PORT} port...`));
+
+
+connectDB().then(() => {
+  server.listen(PORT, () =>
+    console.log(`Server is running on ${PORT} port...`)
+  );
+});
